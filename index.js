12fb46d854d3e4c4631aea92398aa8186b9d159d
mishuk09 <mishukinfo09@gmail.com>
2023-07-15 19:03:22 +0530

    portfolio

--- index.js
+++ index.js
@@ -19,5 +19,23 @@ function sendMail() {
         })
         .catch(err => console.log(err));
 
-}
+};
 
+
+
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
