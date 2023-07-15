fe24ca36034dcc85a6e93709a7fe16415e8bb475
mishuk09 <mishukinfo09@gmail.com>
2023-07-15 15:27:30 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function sendMain() {
+function sendMail() {
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
-
-
     };
 
+    const serviceID = "service_9s3nbof";
+    const templateID = "template_yn6pero";
 
-    const serviceId = "service_9s3nbof"
-    const templateId = "template_yn6pero"
-    emailjs.send(serviceId, templateId, params).than(
-        res => {
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
             document.getElementById("message").value = "";
             console.log(res);
             alert("Your message sent successfully!!")
-        }
-    )
+
+        })
         .catch(err => console.log(err));
+
 }
+
